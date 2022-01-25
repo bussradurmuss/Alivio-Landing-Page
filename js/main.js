3bca329d0266d279fc6b1ae7f6b93199132a89ce
Busra Durmus <bussrabam@outlook.com>
2022-01-25 21:30:04 +0300

    landing page completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const hamburger_menu = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const close_button = document.querySelector(".close-button");
+
+console.log(close_button);
+
+hamburger_menu.addEventListener("click", () => {
+    menu.style.display = "flex";
+});
+
+close_button.addEventListener("click", () => {
+    menu.style.removeProperty("display");
+});
